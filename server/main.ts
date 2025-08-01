bd39ccd865def26cc6167d424ac2538b7fbaf49c
Pallek Kankanamalage Don Gayathra Shan Siriwardana <shansiriwardana@Palleks-MacBook-Air.local>
2025-08-01 20:28:09 +1200

    backend dockerize

--- server/main.ts
+++ server/main.ts
@@ -20,6 +20,15 @@ console.log(`Opening SQLite database at ${dbFilePath}`);
 await Deno.mkdir(path.dirname(dbFilePath), { recursive: true });
 const db = new Database(dbFilePath);
 
+db.exec(`
+  CREATE TABLE IF NOT EXISTS insights (
+    id INTEGER PRIMARY KEY ASC NOT NULL,
+    brand INTEGER NOT NULL,
+    createdAt TEXT NOT NULL,
+    text TEXT NOT NULL
+  );
+`);
+
 console.log("Initialising server");
 
 const router = new oak.Router({
